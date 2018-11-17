e77a355adfaeac64de0a77237f050c50f87fe163
amir <amiremohamadi@yahoo.com>
2018-11-17 08:43:53 +0330

    update

--- js/shrink.js
+++ js/shrink.js
@@ -1,4 +1,4 @@
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollProgress(); scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -10,4 +10,11 @@ function scrollFunction() {
     document.getElementById("logo").style.width = "80px";
     document.getElementById("logo").style.height = "80px";
   }
+}
+
+function scrollProgress() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("progress").style.width = scrolled+"%";
 }

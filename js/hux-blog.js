cdfc4f0a63ee47a22ebedadc62ccbd8ecb19b40d
amir <amiremohamadi@yahoo.com>
2019-03-17 16:43:57 +0330

    new blog!

---
+++ js/hux-blog.js
@@ -0,0 +1,36 @@
+/*!
+ * Clean Blog v1.0.0 (http://startbootstrap.com)
+ * Copyright 2015 Start Bootstrap
+ * Licensed under Apache 2.0 (https://github.com/IronSummitMedia/startbootstrap/blob/gh-pages/LICENSE)
+ */
+
+// Tooltip Init
+$(function() {
+    $("[data-toggle='tooltip']").tooltip();
+});
+
+
+// make all images responsive
+/* 
+ * Unuse by Hux
+ * actually only Portfolio-Pages can't use it and only post-img need it.
+ * so I modify the _layout/post and CSS to make post-img responsive!
+ */
+// $(function() {
+// 	$("img").addClass("img-responsive");
+// });
+
+// responsive tables
+$(document).ready(function() {
+	$("table").wrap("<div class='table-responsive'></div>");
+	$("table").addClass("table");
+});
+
+// responsive embed videos
+$(document).ready(function () { 
+    $('iframe[src*="youtube.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="youtube.com"]').addClass('embed-responsive-item');
+    $('iframe[src*="vimeo.com"]').wrap('<div class="embed-responsive embed-responsive-16by9"></div>');
+	$('iframe[src*="vimeo.com"]').addClass('embed-responsive-item');
+});
+
